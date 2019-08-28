10650ae101f69c96c9fa01b1335d60a7d086370b
Marek Rewers <mrewers@users.noreply.github.com>
2019-08-27 22:46:48 -0400

    Add all create resolvers to write data to the db

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -49,7 +49,17 @@ func (r *mutationResolver) CreateAuthor(ctx context.Context, data model.CreateAu
 		Psuedonym: data.Psuedonym,
 		CreateAt:  time.Now(),
 	}
-	r.authors = append(r.authors, author)
+
+	rows, err := db.LogAndQuery(
+		r.db,
+		"INSERT INTO authors (name, psuedonym, created_at) VALUES ($1, $2, $3) RETURNING id",
+		data.Name, data.Psuedonym, author.CreateAt,
+	)
+
+	if err != nil || !rows.Next() {
+		return author, err
+	}
+
 	return author, nil
 }
 
@@ -66,7 +76,17 @@ func (r *mutationResolver) CreateCurrency(ctx context.Context, data model.Create
 		Ticker:   data.Ticker,
 		CreateAt: time.Now(),
 	}
-	r.currencies = append(r.currencies, currency)
+
+	rows, err := db.LogAndQuery(
+		r.db,
+		"INSERT INTO currencies (name, ticker, created_at) VALUES ($1, $2, $3) RETURNING id",
+		data.Name, data.Ticker, currency.CreateAt,
+	)
+
+	if err != nil || !rows.Next() {
+		return currency, err
+	}
+
 	return currency, nil
 }
 
@@ -85,7 +105,17 @@ func (r *mutationResolver) CreateFile(ctx context.Context, data model.CreateFile
 		Version:    data.Version,
 		CreateAt:   time.Now(),
 	}
-	r.files = append(r.files, file)
+
+	rows, err := db.LogAndQuery(
+		r.db,
+		"INSERT INTO files (coverimage, source, url, version, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+		data.CoverImage, data.Source, data.URL, data.Version, file.CreateAt,
+	)
+
+	if err != nil || !rows.Next() {
+		return file, err
+	}
+
 	return file, nil
 }
 
@@ -104,7 +134,17 @@ func (r *mutationResolver) CreatePaper(ctx context.Context, data model.CreatePap
 		PageNum:     data.PageNum,
 		CreateAt:    time.Now(),
 	}
-	r.papers = append(r.papers, paper)
+
+	rows, err := db.LogAndQuery(
+		r.db,
+		"INSERT INTO papers (title, description, excerpt, page_num, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+		data.Title, data.Description, data.Excerpt, data.PageNum, paper.CreateAt,
+	)
+
+	if err != nil || !rows.Next() {
+		return paper, err
+	}
+
 	return paper, nil
 }
 
@@ -122,7 +162,17 @@ func (r *mutationResolver) CreateUser(ctx context.Context, data model.CreateUser
 		Password: data.Password,
 		CreateAt: time.Now(),
 	}
-	r.users = append(r.users, user)
+
+	rows, err := db.LogAndQuery(
+		r.db,
+		"INSERT INTO users (name, email, password, created_at) VALUES ($1, $2, $3, $4) RETURNING id",
+		data.Name, data.Email, data.Password, user.CreateAt,
+	)
+
+	if err != nil || !rows.Next() {
+		return user, err
+	}
+
 	return user, nil
 }
 
@@ -139,26 +189,49 @@ func (r *queryResolver) Author(ctx context.Context, id string) (*model.Author, e
 	panic("not implemented")
 }
 func (r *queryResolver) Authors(ctx context.Context) ([]*model.Author, error) {
-	return r.authors, nil
+	var author *model.Author
+	var authors []*model.Author
+
+	rows, err := db.LogAndQuery(r.db, "SELECT id, name, psuedonym, created_at FROM authors")
+	defer rows.Close()
+
+	if err != nil {
+		errors.DebugError(err)
+		return nil, errors.InternalServerError
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&author.ID, &author.Name, &author.Psuedonym, &author.CreateAt); err != nil {
+			errors.DebugError(err)
+			return nil, errors.InternalServerError
+		}
+		authors = append(authors, author)
+	}
+
+	return authors, nil
 }
+
 func (r *queryResolver) Currency(ctx context.Context, id string) (*model.Currency, error) {
 	panic("not implemented")
 }
 func (r *queryResolver) Currencies(ctx context.Context) ([]*model.Currency, error) {
 	return r.currencies, nil
 }
+
 func (r *queryResolver) File(ctx context.Context, id string) (*model.File, error) {
 	panic("not implemented")
 }
 func (r *queryResolver) Files(ctx context.Context) ([]*model.File, error) {
 	return r.files, nil
 }
+
 func (r *queryResolver) Paper(ctx context.Context, id string) (*model.Paper, error) {
 	panic("not implemented")
 }
 func (r *queryResolver) Papers(ctx context.Context) ([]*model.Paper, error) {
 	return r.papers, nil
 }
+
 func (r *queryResolver) User(ctx context.Context, id string) (*model.User, error) {
 	panic("not implemented")
 }
