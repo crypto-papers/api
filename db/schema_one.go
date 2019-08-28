2c73ec40c4328ede745393e4e9db5cdef121b9ad
Marek Rewers <mrewers@users.noreply.github.com>
2019-08-27 22:43:49 -0400

    Generate users tables on initalization

--- db/schema_one.go
+++ db/schema_one.go
@@ -16,8 +16,9 @@ func schemaOne(db *sql.DB) {
 	ExecQuery(db, "CREATE TABLE IF NOT EXISTS internal.config (id SERIAL PRIMARY KEY, config_name varchar(255), config_value varchar(255));")
 	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.authors (id uuid DEFAULT uuid_generate_v4 (), name varchar(255), papers uuid [], psuedonym BOOL, created_at TIMESTAMPTZ, PRIMARY KEY (id));")
 	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.currencies (id uuid DEFAULT uuid_generate_v4 (), name varchar(255), ticker varchar(255), created_at TIMESTAMPTZ, PRIMARY KEY (id));")
-	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.files (id uuid DEFAULT uuid_generate_v4 (), coverImage varchar(255), source varchar(255), url varchar(255), created_at TIMESTAMPTZ, PRIMARY KEY (id));")
+	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.files (id uuid DEFAULT uuid_generate_v4 (), coverImage varchar(255), source varchar(255), url varchar(255), version numeric, created_at TIMESTAMPTZ, PRIMARY KEY (id));")
 	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.papers (id uuid DEFAULT uuid_generate_v4 (), author uuid [], currency uuid [], description TEXT, excerpt TEXT, page_num SMALLINT, title varchar(255), created_at TIMESTAMPTZ, PRIMARY KEY (id));")
+	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.users (id uuid DEFAULT uuid_generate_v4 (), name varchar(255), email varchar(255), password varchar(255), created_at TIMESTAMPTZ, PRIMARY KEY (id));")
 
 	// Insert default values
 	ExecQuery(db, fmt.Sprintf("SET timezone = '%s';", conf.Base.Timezone))
