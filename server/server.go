60c20877adff58ef2f37da31267517085e98af43
Marek Rewers <mrewers@users.noreply.github.com>
2019-09-01 15:44:06 -0400

    Add custom router to set CORS headers

--- server/server.go
+++ server/server.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 
 	"github.com/99designs/gqlgen/handler"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
+
 	"github.com/crypto-papers/api/config"
 	postgres "github.com/crypto-papers/api/db"
 	gen "github.com/crypto-papers/api/generated"
@@ -21,12 +24,25 @@ func StartServer() {
 
 	postgres.CheckSchemaVersion(db)
 
-	http.Handle("/", handler.Playground("Cryptopapers", "/query"))
-	http.Handle("/query", handler.GraphQL(gen.NewExecutableSchema(res.NewRootResolvers(db))))
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:4000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
+	router.Handle("/", handler.Playground("Cryptopapers", "/query"))
+	router.Handle("/query", handler.GraphQL(
+		gen.NewExecutableSchema(res.NewRootResolvers(db)),
+	))
 
 	log.Printf("Server is running on http://localhost:%s", port)
 
-	err := http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+port, router)
 	handleErr(err)
 }
 
