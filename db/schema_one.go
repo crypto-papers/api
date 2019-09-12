65928316c0c0648d0efcdce8c11df4fd4928e2ab
Marek Rewers <mrewers@users.noreply.github.com>
2019-09-12 00:31:58 -0400

    Add author-paper connection table

--- db/schema_one.go
+++ db/schema_one.go
@@ -19,6 +19,8 @@ func schemaOne(db *sql.DB) {
 	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.files (id uuid DEFAULT uuid_generate_v4 (), cover_image VARCHAR(255), latest BOOL, pub_date TIMESTAMPTZ, source VARCHAR(255), url VARCHAR(255), version NUMERIC, created_at TIMESTAMPTZ, PRIMARY KEY (id));")
 	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.papers (id uuid DEFAULT uuid_generate_v4 (), author uuid [], asset uuid [], description TEXT, excerpt TEXT, file uuid [], page_num SMALLINT, pretty_id SERIAL, title VARCHAR(255), created_at TIMESTAMPTZ, PRIMARY KEY (id));")
 	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.users (id uuid DEFAULT uuid_generate_v4 (), name VARCHAR(255), email VARCHAR(255) NOT NULL UNIQUE, password TEXT NOT NULL, created_at TIMESTAMPTZ, PRIMARY KEY (id));")
+	// Create connection tables
+	ExecQuery(db, "CREATE TABLE IF NOT EXISTS public.author_paper (author_id uuid REFERENCES authors (id) ON UPDATE CASCADE, paper_id uuid REFERENCES papers (id) ON UPDATE CASCADE, CONSTRAINT author_paper_pkey PRIMARY KEY (author_id, paper_id));")
 
 	// Insert default values
 	ExecQuery(db, fmt.Sprintf("SET timezone = '%s';", conf.Base.Timezone))
